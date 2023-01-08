3b15fc37f6e518d488ac4647d3ee997dcbad3c05
Osmar Ortiz <52983571+Xizted@users.noreply.github.com>
2023-01-08 15:10:25 -0400

    fixed dropdown

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,54 +1,39 @@
 @extends('layouts.app')
-<nav class="navbar navbar-expand-md navbar-light bg-secondary  shadow">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary flex-row flex-nowrap justify-content-start shadow">
     <div class="container">
-        <a class="navbar-brand text-white" href="{{ url('/') }}">
-            {{-- {{ config('app.name', 'Laravel') }} --}}
-            Sistema de Control de Visitantes
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <div>
+            <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <a class="navbar-brand text-white" href="{{ url('/') }}">
+                {{-- {{ config('app.name', 'Laravel') }} --}}
+                Sistema de Control de Visitantes
+            </a>
+        </div>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
+        <div class="navbar-collapse d-flex">
             <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @if (Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-                @endif
-                @else
-                <li class="nav-item dropdown">
+            <ul class="navbar-nav ml-auto flex-row">
+                <li class=" dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->getFullNameAttribute() }}
                     </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <div class="position-absolute dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
-
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
                         </form>
                     </div>
                 </li>
-                @endguest
             </ul>
         </div>
     </div>
 </nav>
+
+{{-- Sidebar --}}
 <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-secondary shadow">
     <div class="position-sticky">
         <div class="list-group list-group-flush mx-3 mt-4">
